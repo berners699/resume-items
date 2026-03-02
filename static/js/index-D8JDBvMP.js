79f90958c6fac12f8851529b5c8ef5a665814f01
berners699 <berners699@users.noreply.github.com>
2026-03-02 19:02:36 +0000

    deploy: ab492ecc458f58782993548d8350bdaebd8f28bf

---
+++ static/js/index-D8JDBvMP.js
@@ -0,0 +1,2 @@
+import{c as e,o}from"./index-sDlIPEd-.js";const t={class:"home"},_=Object.assign({name:"AboutComp"},{__name:"index",setup(s){return(c,a)=>(o(),e("div",t,"关于"))}});export{_ as default};
+//# sourceMappingURL=index-D8JDBvMP.js.map
--- static/js/index-DupAcF_C.js
+++
@@ -1,2 +0,0 @@
-import{c as e,o}from"./index-BnL8t-v8.js";const t={class:"home"},_=Object.assign({name:"AboutComp"},{__name:"index",setup(s){return(c,a)=>(o(),e("div",t,"关于"))}});export{_ as default};
-//# sourceMappingURL=index-DupAcF_C.js.map
